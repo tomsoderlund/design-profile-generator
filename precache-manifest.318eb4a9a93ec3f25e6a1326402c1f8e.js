ea989a120d6c71109f7576b6e2f1d25e523c7035
Tom Söderlund <tom@tomsoderlund.com>
2019-10-02 19:18:58 +0200

    Updates

---
+++ precache-manifest.318eb4a9a93ec3f25e6a1326402c1f8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f998d97185c4cd3aa2c540539625da",
+    "url": "/design-profile-generator/index.html"
+  },
+  {
+    "revision": "f2f81d397c32eaa71c3f",
+    "url": "/design-profile-generator/static/css/main.33971c47.chunk.css"
+  },
+  {
+    "revision": "f114125ca9bcb03e875e",
+    "url": "/design-profile-generator/static/js/2.485ae671.chunk.js"
+  },
+  {
+    "revision": "f2f81d397c32eaa71c3f",
+    "url": "/design-profile-generator/static/js/main.5359c283.chunk.js"
+  },
+  {
+    "revision": "c6cfe4b5f99a60f055ec",
+    "url": "/design-profile-generator/static/js/runtime-main.c608733b.js"
+  }
+]);
