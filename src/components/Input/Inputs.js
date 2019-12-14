ca03be45a64101f11705c8f7df2230d3b68d36ff
Tom Söderlund <tom@tomsoderlund.com>
2019-12-14 19:30:05 +0100

    Refactor Inputs

--- src/components/Input/Inputs.js
+++ src/components/Input/Inputs.js
@@ -14,6 +14,8 @@ const allFonts = googleFonts.items.sort((a, b) => (a.category > b.category) ? 1 
   value: item.family
 }))
 
+const fontSizes = ['10px', '12px', '14px', '16px', '18px', '20px']
+
 const fontWeights = [
   { name: 'Thin (100)', value: 100 },
   { name: 'Light (300)', value: 300 },
@@ -25,106 +27,133 @@ const fontWeights = [
   { name: 'Black (900)', value: 900 }
 ]
 
-const inputCategories = [
-  {
-    title: 'Colors',
-    fields: [
-      {
-        name: 'backgroundColor',
-        title: 'Background',
-        type: 'color'
-      },
-      {
-        name: 'actionColor',
-        title: 'Action/Primary',
-        type: 'color'
-      },
-      {
-        name: 'headerColor',
-        title: 'Header/Secondary',
-        type: 'color'
-      },
-      {
-        name: 'textColor',
-        title: 'Text',
-        type: 'color'
-      },
-      {
-        name: 'headlineColor',
-        title: 'Headlines',
-        type: 'color'
-      },
-      {
-        name: 'warningColor',
-        title: 'Warning',
-        type: 'color'
-      },
-      {
-        name: 'informationColor',
-        title: 'Information',
-        type: 'color'
-      }
-    ]
+const inputTypes = {
+  boolean: {
+    type: 'checkbox'
   },
-  {
-    title: 'Text',
-    fields: [
-      {
-        name: 'textFont',
-        title: 'Typeface',
-        type: 'select',
-        options: allFonts
-      },
-      {
-        name: 'textSize',
-        title: 'Size',
-        type: 'select',
-        options: ['10px', '12px', '14px', '16px', '18px', '20px']
-      },
-      {
-        name: 'textWeight',
-        title: 'Weight',
-        type: 'select',
-        options: fontWeights
-      }
-    ]
+  color: {
+    type: 'color'
   },
-  {
-    title: 'Headlines',
-    fields: [
-      {
-        name: 'headlineFont',
-        title: 'Typeface',
-        type: 'select',
-        options: allFonts
-      },
-      {
-        name: 'headlineWeight',
-        title: 'Weight',
-        type: 'select',
-        options: fontWeights
-      },
-      {
-        name: 'headlineItalic',
-        title: 'Italic',
-        type: 'checkbox'
-      },
-      {
-        name: 'headlineUppercase',
-        title: 'Uppercase',
-        type: 'checkbox'
-      }
-    ]
+  'font-family': {
+    type: 'select',
+    options: allFonts
+  },
+  'font-weight': {
+    type: 'select',
+    options: fontWeights
+  },
+  'font-size': {
+    type: 'select',
+    options: fontSizes
   }
+}
+
+const allColorProps = {
+  title: 'Colors',
+  fields: [
+    {
+      name: 'backgroundColor',
+      title: 'Background',
+      input: 'color'
+    },
+    {
+      name: 'actionColor',
+      title: 'Action/Primary',
+      input: 'color'
+    },
+    {
+      name: 'headerColor',
+      title: 'Header/Secondary',
+      input: 'color'
+    },
+    {
+      name: 'textColor',
+      title: 'Text',
+      input: 'color'
+    },
+    {
+      name: 'headlineColor',
+      title: 'Headlines',
+      input: 'color'
+    },
+    {
+      name: 'warningColor',
+      title: 'Warning',
+      input: 'color'
+    },
+    {
+      name: 'informationColor',
+      title: 'Information',
+      input: 'color'
+    }
+  ]
+}
+
+const textProps = {
+  title: 'Text',
+  fields: [
+    {
+      name: 'textFont',
+      title: 'Typeface',
+      input: 'font-family'
+    },
+    {
+      name: 'textSize',
+      title: 'Size',
+      input: 'font-size'
+    },
+    {
+      name: 'textWeight',
+      title: 'Weight',
+      input: 'font-weight'
+    }
+  ]
+}
+
+const headlinesProps = {
+  title: 'Headlines',
+  fields: [
+    {
+      name: 'headlineFont',
+      title: 'Typeface',
+      input: 'font-family'
+    },
+    {
+      name: 'headlineWeight',
+      title: 'Weight',
+      input: 'font-weight'
+    },
+    {
+      name: 'headlineItalic',
+      title: 'Italic',
+      input: 'boolean'
+    },
+    {
+      name: 'headlineUppercase',
+      title: 'Uppercase',
+      input: 'boolean'
+    }
+  ]
+}
+
+const allInputCategories = [
+  allColorProps,
+  textProps,
+  headlinesProps
 ]
 
-const SmartInput = (props) => props.type === 'select'
-  ? <SelectDropdown {...props} style={{ width: '8em' }} />
-  : <input {...props} value={props.value || (props.type === 'color' ? '#ffffff' : '')} />
+const SmartInput = (props) => {
+  const inputType = inputTypes[props.input]
+  return inputType.type === 'select'
+    ? <SelectDropdown {...inputType} {...props} style={{ width: '8em' }} />
+    : <input {...inputType} {...props} value={props.value || (props.type === 'color' ? '#ffffff' : '')} />
+}
 
-export default ({ profile, setProfile }) => {
+const CategoriesAndInputs = ({ categories, profile, setProfile }) => {
   const handleChange = (fieldName, event) => {
-    const newValue = Object.prototype.hasOwnProperty.call(event.target, 'checked') ? event.target.checked : event.target.value
+    const newValue = Object.prototype.hasOwnProperty.call(event.target, 'checked')
+      ? event.target.checked
+      : event.target.value
     setProfile({
       ...profile,
       [fieldName]: newValue
@@ -132,22 +161,28 @@ export default ({ profile, setProfile }) => {
   }
 
   return (
+    categories.map(category => (
+      <div key={category.title}>
+        <h3>{category.title}</h3>
+        {category.fields.map(field => (
+          <p key={field.name}>
+            <label>
+              {field.title}:{' '}
+              <SmartInput input={field.input} value={profile[field.name]} onChange={event => handleChange(field.name, event)} />
+            </label>
+          </p>
+        ))}
+      </div>
+    ))
+  )
+}
+
+export default ({ profile, setProfile }) => {
+  return (
     <DivLeft>
       <Themes setProfile={setProfile} />
       <h2>Details</h2>
-      {inputCategories.map(category => (
-        <div key={category.title}>
-          <h3>{category.title}</h3>
-          {category.fields.map(field => (
-            <p key={field.name}>
-              <label>
-                {field.title}:{' '}
-                <SmartInput type={field.type} options={field.options} value={profile[field.name]} onChange={event => handleChange(field.name, event)} />
-              </label>
-            </p>
-          ))}
-        </div>
-      ))}
+      <CategoriesAndInputs categories={allInputCategories} profile={profile} setProfile={setProfile} />
       <GoogleFontLink family={profile.textFont} />
       <GoogleFontLink family={profile.headlineFont} />
     </DivLeft>
