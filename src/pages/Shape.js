890c4fd34df3142d19f28a8ea857c06508b24af5
Tom Söderlund <tom@tomsoderlund.com>
2020-10-09 09:25:13 +0200

    Sections prepare

--- src/pages/Shape.js
+++ src/pages/Shape.js
@@ -6,10 +6,37 @@ import Page from '../components/page/Page'
 import SmartInput from '../components/input/SmartInput'
 
 const inputMetadata = {
-  backgroundColor: { type: 'color' },
-  cornerRadius: { min: 0, max: 2, step: 0.1 },
+  colorsBackground: { type: 'color' },
+  cornersRadius: { min: 0, max: 2, step: 0.1 },
   roundnessY: { min: -3 },
   reflectionY: { min: -3 }
+}
+
+const styleSections = {
+  colors: ['colorsBackground'],
+  corners: ['cornersRadius'],
+  roundness: ['roundnessIntensity', 'roundnessY'],
+  reflection: ['reflectionIntensity', 'reflectionY', 'reflectionSize', 'reflectionBlur'],
+  shadow: ['shadowIntensity', 'shadowY', 'shadowSize', 'shadowBlur']
+}
+
+const defaultStyleProperties = {
+  colorsBackground: '#00ced1',
+
+  cornersRadius: '0.5em',
+
+  roundnessIntensity: 0.5,
+  roundnessY: '100%',
+
+  reflectionIntensity: 0.3,
+  reflectionY: '0.2em',
+  reflectionSize: '0em',
+  reflectionBlur: '0.125em',
+
+  shadowIntensity: 0.3,
+  shadowY: '0.2em',
+  shadowSize: '0em',
+  shadowBlur: '0.125em'
 }
 
 const defaultValuesForFormat = {
@@ -26,28 +53,12 @@ const guessFormat = (value) => {
 }
 
 const kebabCase = (str) => str.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()
+const titleCase = str => str.replace(/(?:^|\s|[-"'([{])+\S/g, (c) => c.toUpperCase())
 
 const get = (obj, key, defaultValue) => (obj && Object.prototype.hasOwnProperty.call(obj, key)) ? obj[key] : defaultValue
 
 const ShapePage = () => {
-  const [inputs, setInputs] = useState({
-    backgroundColor: '#00ced1',
-
-    cornerRadius: '0.5em',
-
-    roundnessIntensity: 0.5,
-    roundnessY: '100%',
-
-    reflectionIntensity: 0.3,
-    reflectionY: '0.2em',
-    reflectionSize: '0em',
-    reflectionBlur: '0.125em',
-
-    shadowIntensity: 0.3,
-    shadowY: '0.2em',
-    shadowSize: '0em',
-    shadowBlur: '0.125em'
-  })
+  const [inputs, setInputs] = useState(defaultStyleProperties)
 
   const handleInputChange = ({ target }) => {
     const value = target.type === 'checkbox' ? target.checked : target.value
@@ -55,17 +66,40 @@ const ShapePage = () => {
   }
 
   const buttonStyle = {
-    borderRadius: inputs.cornerRadius,
+    borderRadius: inputs.cornersRadius,
     boxShadow: [
       ...(inputs.shadowIntensity > 0 ? [`0 ${inputs.shadowY} ${inputs.shadowBlur} ${inputs.shadowSize} rgba(0, 0, 0, ${inputs.shadowIntensity})`] : []),
       ...(inputs.reflectionIntensity > 0 ? [`inset 0 ${inputs.reflectionY} ${inputs.reflectionBlur} ${inputs.reflectionSize} rgba(255, 255, 255, ${inputs.reflectionIntensity})`] : [])
     ].join(', '),
     background: [
       ...(inputs.roundnessIntensity > 0 ? [`linear-gradient(180deg, rgba(255,255,255, ${inputs.roundnessIntensity}) 0%, rgba(255,255,255, 0) ${inputs.roundnessY})`] : []),
-      inputs.backgroundColor
+      inputs.colorsBackground
     ].join(', '),
-    color: tinycolor(inputs.backgroundColor).getBrightness() > 128 ? 'black' : 'white'
+    color: tinycolor(inputs.colorsBackground).getBrightness() > 128 ? 'black' : 'white'
   }
+
+  const StyleSection = ({ sectionName, propertyNames }) => (
+    <div>
+      <h3>{titleCase(sectionName)}</h3>
+      {propertyNames.map((propertyKey, index) => {
+        const format = get(inputMetadata[propertyKey], 'format', guessFormat(inputs[propertyKey]))
+        return (
+          <SmartInput
+            key={propertyKey}
+            type={get(inputMetadata[propertyKey], 'type')}
+            name={propertyKey}
+            label={propertyKey.replace(sectionName, '')}
+            format={format}
+            min={get(inputMetadata[propertyKey], 'min', defaultValuesForFormat[format].min)}
+            max={get(inputMetadata[propertyKey], 'max', defaultValuesForFormat[format].max)}
+            step={get(inputMetadata[propertyKey], 'step', defaultValuesForFormat[format].step)}
+            value={inputs[propertyKey]}
+            onChange={handleInputChange}
+          />
+        )
+      })}
+    </div>
+  )
 
   return (
     <Page>
@@ -74,12 +108,23 @@ const ShapePage = () => {
           <button style={buttonStyle}>Button</button>
         </div>
 
-        <div>
+        {/*
+        <div className='sections-box'>
+          {Object.keys(styleSections).map((sectionName, index) => (
+            <StyleSection
+              key={sectionName}
+              sectionName={sectionName}
+              propertyNames={styleSections[sectionName]}
+            />
+          ))}
+        </div>
+ */}
+        <div className='properties-box'>
           {Object.keys(inputs).map((propertyKey, index) => {
             const format = get(inputMetadata[propertyKey], 'format', guessFormat(inputs[propertyKey]))
             return (
               <SmartInput
-                key={index}
+                key={propertyKey}
                 type={get(inputMetadata[propertyKey], 'type')}
                 name={propertyKey}
                 label={propertyKey}
